be46325c3befe29786e77d1a012bf317efb52bbb
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-11 09:34:43 +0100

    render current smurfs to screen

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import SmurfCard from "./SmurfCard";
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
+
+const smurfsApi = "http://localhost:3333/smurfs";
+
+export function SmurfList({ smurfs, getSmurfs, deleteSmurf }) {
+  useEffect(() => {
+    getSmurfs(smurfsApi);
+  }, []);
+  return (
+    <div className="smurfList">
+      {smurfs.map(smurf => (
+        <SmurfCard smurf={smurf} key={smurf.id} />
+      ))}
+    </div>
+  );
+}
+
+export default connect(
+  state => state,
+  actionCreators
+)(SmurfList);
--- smurfs/src/state/actionCreators.js
+++ smurfs/src/state/actionCreators.js
@@ -1,26 +1,28 @@
-// import axios from 'axios';
+import axios from 'axios';
 import * as types from './actionTypes';
 
-// const smurfsApi = "http://localhost:3333/smurfs";
+const smurfsApi = "http://localhost:3333/smurfs";
 
-// export function addSmurfs(smurfs){
-//     return {
-//         type: types.ADD_SMURFS,
-//         payload: smurfs,
-//     }
-// };
+export function addSmurfs(smurfs){
+    return {
+        type: types.ADD_SMURFS,
+        payload: smurfs,
+    }
+};
 
-// export function getSmurfs = () => dispatch => {
-//     axios.get(smurfsApi)
-//     .then(response => {
-//         const smurfs = response.data
-//         dispatch(addSmurfs(smurfs));
+export const getSmurfs = () => dispatch => {
+    axios.get(smurfsApi)
+    .then(response => {
+        const smurfs = response.data
+        dispatch(addSmurfs(smurfs));
 
-//     })
-//     .catch(error => {
-//         console.log("Uh oh - there's an error with the Api!", error.message)
-//     })
-// };
+    })
+    .catch(error => {
+        console.log("Uh oh - there's an error with the Api!", error.message)
+    })
+};
+
+
 export function increment() {
     return { type: types.INCREMENT };
   }
