dd9d9bd1b19b61c74ab901931c3d191d87c45c20
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-11 13:32:32 +0100

    create nav

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     
       <div className="App">
+        <Navbar />
         Smurfs will render here
       </div>
      
