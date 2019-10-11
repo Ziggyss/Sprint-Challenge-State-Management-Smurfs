7646727d6b8c0b15f58fc1472c58efe4dc9db501
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-11 13:46:08 +0100

    create monster reducer, create store and wrap in Provider

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import * as reducers from "./state/reducers";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+const monsterReducer = combineReducers({
+  smurfs: reducers.smurfsReducer,
+  formValues: reducers.formReducer
+});
+
+const store = createStore(
+  monsterReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 function App() {
   return (
-    
+    <Provider store={store}>
       <div className="App">
         <Navbar />
         Smurfs will render here
       </div>
-     
+    </Provider>
   );
 }
 
