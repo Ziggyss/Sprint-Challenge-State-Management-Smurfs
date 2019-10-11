db017dfaaafacc0e8eb38f553dbe597d3e482bcb
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-11 13:20:35 +0100

    clean slate

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import thunk from "redux-thunk";
-import { combineReducers, createStore, compose, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import * as reducers from "./state/reducers";
-import SmurfList from './components/SmurfList';
-import Form from './components/Form';
 
-// import Counter from "../src/components/counter";
-
-const monsterReducer = combineReducers({
-  smurfs: reducers.smurfsReducer,
-  formValues: reducers.formReducer,
-//   count: reducers.countReducer
- });
-
-const store = createStore(
-  monsterReducer,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        {/* <Counter /> */}
-        <SmurfList />
-        <Form />
-      </div>
-    </Provider>
+    
+      <div className="App"></div>
+     
   );
 }
 
