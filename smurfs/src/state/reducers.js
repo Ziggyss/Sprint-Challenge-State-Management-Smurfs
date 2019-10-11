947cf5cfeff6c640daad029529b49d13243425b7
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-11 10:51:12 +0100

    latest code with 10 minutes go to

--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -38,16 +38,4 @@ export function formReducer(state = initialFormState, action) {
   }
 }
 
-// const initialValueCount = 0;
-// export function countReducer(count = initialValueCount, action) {
-//   switch (action.type) {
-//     case types.INCREMENT:
-//       return count + 1;
-//     case types.DECREMENT:
-//       return count - 1;
-//     case types.RESET:
-//       return 0;
-//     default:
-//       return count;
-//   }
-// }
+
